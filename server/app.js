a4fd326006b8fb385312ea5800f7749df65bd356
Navneet Prajapati <144175174+NavneetPrajapati31@users.noreply.github.com>
2025-02-15 05:03:31 +0530

    Update app.js

--- server/app.js
+++ server/app.js
@@ -18,10 +18,7 @@ const orders = require("./routes/orders.js");
 
 app.use(
   cors({
-    origin: [
-      "http://localhost:5173",
-      "https://kashvi-creation-psi.vercel.app/",
-    ],
+    origin: ["https://kashvi-creation-psi.vercel.app/"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
