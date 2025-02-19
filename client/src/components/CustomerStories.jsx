63673ed08344573b5299bba0d84b17288957dbac
Navneet Prajapati <144175174+NavneetPrajapati31@users.noreply.github.com>
2025-02-19 19:35:21 +0530

    update

--- client/src/components/CustomerStories.jsx
+++ client/src/components/CustomerStories.jsx
@@ -7,44 +7,47 @@ import ArrowBackIosNewOutlinedIcon from "@mui/icons-material/ArrowBackIosNewOutl
 const reviews = [
   {
     name: "Sant Das",
-    location: "Sydney, Australia",
+    location: "Surat, India",
     review:
       "Every detail wowed! At the wedding, it stole the show; friends are eyeing KASHVI's collection online after this buzz.",
+    link: "https://www.koskii.com/cdn/shop/files/koskii-purple-stonework-semicrepe-designer-saree-saus0034824_purple_1_1_1400x.jpg?v=1719829414",
   },
   {
     name: "Romika Nigam",
-    location: "Mumbai, India",
+    location: "Surat, India",
     review:
       "KASHVI made my bridal journey a dream! Stunning bridal wear, impeccable service; felt extraordinary, like a princess.",
+    link: "https://th.bing.com/th/id/R.c19d879cd1482e7efd496a14cc3273a2?rik=DHmYFMrkXq7qvQ&riu=http%3a%2f%2fwww.koskii.com%2fcdn%2fshop%2fproducts%2fkoskii-sea-green-stonework-satin-designer-saree-saus0022632_sea_green__1.jpg%3fv%3d1670485220&ehk=Hkig7iWqkVfwtyPWVdW3RB%2fuaQ1Vkp6%2f4ibOu5XI4ig%3d&risl=&pid=ImgRaw&r=0",
   },
   {
     name: "Arjun Mehta",
-    location: "New Delhi, India",
+    location: "Surat, India",
     review:
       "The craftsmanship is just amazing. My wedding look was elevated with the perfect outfit from KASHVI.",
+    link: "https://www.koskii.com/cdn/shop/products/koskii-black-sequins-georgette-designer-saree-saus0021890_black_4_1800x1800.jpg?v=1663334015",
   },
   {
     name: "Meera Kapoor",
-    location: "London, UK",
+    location: "Surat, India",
     review:
       "I felt like a queen in my bridal lehenga. The attention to detail is truly outstanding!",
+    link: "https://www.koskii.com/cdn/shop/products/koskii-lavender-mirrorwork-net-designer-saree-saus0021303_lavender_3_1800x1800.jpg?v=1661854696",
   },
   {
     name: "Rajiv Sharma",
-    location: "Toronto, Canada",
+    location: "Surat, India",
     review:
       "Best wedding collection ever! My suit was elegant, stylish, and perfectly tailored.",
+    link: "https://www.koskii.com/cdn/shop/products/koskii-peach-threadwork-semi-crepe-designer-saree-saus0022636_peach__4_1400x.jpg?v=1670488483",
   },
   {
     name: "Aisha Khan",
-    location: "Dubai, UAE",
+    location: "Surat, India",
     review:
       "Absolutely stunning outfits! I received so many compliments on my wedding day.",
+    link: "https://www.koskii.com/cdn/shop/files/koskii-white-sequins-shimmergeorgette-designer-saree-saus0031899_white_1_1800x1800.jpg?v=1693978281",
   },
 ];
-
-const imageURL =
-  "https://www.koskii.com/cdn/shop/files/koskii-purple-stonework-semicrepe-designer-saree-saus0034824_purple_1_1_1400x.jpg?v=1719829414";
 
 const CustomerStories = () => {
   const [currentIndex, setCurrentIndex] = useState(0);
@@ -101,7 +104,7 @@ const CustomerStories = () => {
                 .map((review, index) => (
                   <div key={index} className="review-box">
                     <img
-                      src={imageURL}
+                      src={review.link}
                       alt="Customer"
                       className="review-image"
                     />
